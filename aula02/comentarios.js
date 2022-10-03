5ec96eae2ce0246699857b4d798a21aae37a48a6
Rafael Martinez do Carmo <rafaelmdocarmo@gmail.com>
2022-10-03 16:41:13 -0300

    First Commit

---
+++ aula02/comentarios.js
@@ -0,0 +1,9 @@
+// Escrever um comentário
+
+console.log("Olá Mundo"); // Aqui tem outro comentário
+
+/*
+
+comentário com mais de uma linha
+
+*/
---
+++ aula04/index.js
@@ -0,0 +1,13 @@
+let nome = "João";
+
+console.log(`${nome} nasceu em 1984.`);
+console.log(`Em 2000 ${nome} conheceu Maria.`);
+console.log(`${nome} casou-se com Maria em 2012.`);
+console.log(`Maria teve 1 filho com ${nome} em 2015.`);
+console.log(`O filho de ${nome} se chama Eduardo.`);
+
+let nome2;
+nome2 = "Rafael";
+console.log(nome2);
+nome2 = "Gabriel";
+console.log(nome2);
