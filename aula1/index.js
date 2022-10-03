c49487437400fbf6427afbd8b93474105ac3c655
Rafael Martinez do Carmo <110850303+rafaeldocarmo@users.noreply.github.com>
2022-10-03 16:52:34 -0300

    Delete aula1 directory

--- aula1/index.js
+++
@@ -1,7 +0,0 @@
-console.log("Rafael do Carmo"); // Strings
-console.log('Rafael do Carmo');
-console.log(`Rafael do Carmo`);
-
-console.log(15.85); // Numbers
-
-console.log(15.85, `Rafael do Carmo`);
