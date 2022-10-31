ed0453505e31a82af9e87249869cb52c80362251
Rafael Martinez do Carmo <rafaelmdocarmo@gmail.com>
2022-10-31 14:50:59 -0300

    Commit

--- BÁSICO/Calculadora IMC/main.js
+++ BÁSICO/Calculadora IMC/main.js
@@ -1,21 +1,21 @@
 // Capturar evento de submit do formulário
-const form = document.querySelector('#formulario');
+const form = document.querySelector("#formulario");
 
-form.addEventListener('submit', function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  const inputPeso = e.target.querySelector('#peso');
-  const inputAltura = e.target.querySelector('#altura');
+  const inputPeso = e.target.querySelector("#peso");
+  const inputAltura = e.target.querySelector("#altura");
 
   const peso = Number(inputPeso.value);
   const altura = Number(inputAltura.value);
 
   if (!peso) {
-    setResultado('Peso inválido', false);
+    setResultado("Peso inválido", false);
     return;
   }
 
   if (!altura) {
-    setResultado('Altura inválida', false);
+    setResultado("Altura inválida", false);
     return;
   }
 
@@ -27,9 +27,15 @@ form.addEventListener('submit', function (e) {
   setResultado(msg, true);
 });
 
-function getNivelImc (imc) {
-  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
-    'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+function getNivelImc(imc) {
+  const nivel = [
+    "Abaixo do peso",
+    "Peso normal",
+    "Sobrepeso",
+    "Obesidade grau 1",
+    "Obesidade grau 2",
+    "Obesidade grau 3",
+  ];
 
   if (imc >= 39.9) return nivel[5];
   if (imc >= 34.9) return nivel[4];
@@ -39,26 +45,26 @@ function getNivelImc (imc) {
   if (imc < 18.5) return nivel[0];
 }
 
-function getImc (peso, altura) {
+function getImc(peso, altura) {
   const imc = peso / altura ** 2;
   return imc.toFixed(2);
 }
 
-function criaP () {
-  const p = document.createElement('p');
+function criaP() {
+  const p = document.createElement("p");
   return p;
 }
 
-function setResultado (msg, isValid) {
-  const resultado = document.querySelector('#resultado');
-  resultado.innerHTML = '';
+function setResultado(msg, isValid) {
+  const resultado = document.querySelector("#resultado");
+  resultado.innerHTML = "";
 
   const p = criaP();
 
   if (isValid) {
-    p.classList.add('paragrafo-resultado');
+    p.classList.add("paragrafo-resultado");
   } else {
-    p.classList.add('bad');
+    p.classList.add("bad");
   }
 
   p.innerHTML = msg;
--- BÁSICO/Operação Ternária/main.js
+++ BÁSICO/Operação Ternária/main.js
@@ -3,6 +3,6 @@ const pontuacaoUSuario = 1000;
 const nivelUsuario = pontuacaoUSuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
 
 const corUsuario = 'Pink';
-const corPadrao = corUsuario || 'Preta';
+const corPadrao = corUsuario || 'Preto';
 
 console.log(nivelUsuario, corPadrao);
